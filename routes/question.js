e45271fc4e9ff0dadd4b12d9acb7846e592fdd6c
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-03-28 19:06:55 +0200

    working on routes

--- routes/question.js
+++ routes/question.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const isAuthenticated = require('../middlewares/jwt');
 
-// @desc    Gets question from DB
+// @desc    Gets random question from DB
 // @route   GET /question
 // @access  Must be authenticated
 router.get('/', isAuthenticated, async (req, res, next) => {
-    res.send('This is the REST API home. Add an endpoint to see data.')
+    try {
+            const question = await QuestionModel.aggregate([{ $sample: { size: 1 } }]);
+            if (!question) {
+                return res.status(404).json({ msg: 'No questions found' });
+            }
+            res.json(question);
+        }   catch (err) {
+                next(err);
+    }
 });
 
 // @desc    Posts new question to DB
