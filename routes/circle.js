0d0f50b54e76661b11a4d8517876c09f2375f868
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-04-01 10:04:38 +0200

    auth, categories, circle routes done

--- routes/circle.js
+++ routes/circle.js
@@ -19,6 +19,24 @@ router.get('/', isAuthenticated, async (req, res, next) => {
   }
 });
 
+// @desc    Gets all circle admins
+// @route   GET /circle/admins/:circleId
+// @access  Must be authenticated
+router.get('/admins/:circleId', isAuthenticated, async (req, res, next) => {
+  const { circleId } = req.params;
+  try {
+    const circle = await Circle.findById(circleId);
+    if (!circle) {
+      return res.status(404).json({ msg: 'Circle not found' });
+    }
+    const admins = circle.admins;
+    res.status(200).json(admins);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
 // @desc    Create a new circle
 // @route   POST /circle
 // @access  Must be authenticated
@@ -37,12 +55,12 @@ router.post('/create', isAuthenticated, async (req, res, next) => {
         admins: [req.payload._id]
       });
       //we add the circle to the user's property 'circles' as well
-      const addCircleToUser = await User.findByIdAndUpdate(req.payload._id, {
+      const updatedUser = await User.findByIdAndUpdate(req.payload._id, {
         $addToSet: {
           'circles': circle._id
         }
       })
-      res.status(200).json(circle);
+      res.status(200).json({circle, updatedUser});
     }
   } catch (err) {
     next(err);
@@ -50,9 +68,9 @@ router.post('/create', isAuthenticated, async (req, res, next) => {
 });
 
 // @desc    Allows a user to exit a circle
-// @route   PUT /circle/exit/:circleId
+// @route   PATCH /circle/exit/:circleId
 // @access  Must be authenticated
-router.put('/exit/:circleId', isAuthenticated, async (req, res, next) => {
+router.patch('/exit/:circleId', isAuthenticated, async (req, res, next) => {
   const { circleId } = req.params;
   try { 
     const user = await User.findById(req.payload._id);
@@ -60,8 +78,12 @@ router.put('/exit/:circleId', isAuthenticated, async (req, res, next) => {
       res.status(404).json({ success: false, message: 'User not found' });
       return;
     }
+    if (!user.circles.includes(circleId)) {
+      res.status(404).json({success: false, message: 'User does not belong to the circle'});
+      return;
+    }
     const updatedUser = await User.findByIdAndUpdate(req.payload._id, {
-      $pull: { 'circles': circle._id }
+      $pull: { 'circles': circleId }
     }, { new: true });
     res.status(201).json(updatedUser);
   } catch (err) {
